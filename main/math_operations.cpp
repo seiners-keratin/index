1d8ac95c6e334ab4148e339d9954bdbafd18d053
Robert McGuire <152697310+seiners-keratin@users.noreply.github.com>
2023-12-02 14:55:07 +0300

    Create math_operations.cpp

---
+++ main/math_operations.cpp
@@ -0,0 +1,10 @@
+// Implementation of math operations
+#include "math_operations.h" // Include the header file
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
